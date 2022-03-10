6004a8019bf9564160244aa70ef97636a36e5b53
sam <samuelnega1@gmail.com>
2022-03-10 14:03:00 -0800

    update

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,11 +6,11 @@
  */
 int main(void)
 {
-printf("Size of a char:%d,sizeof(char);
-printf("Size of an int:%d,sizeof(int);
-printf("Size of a long int:%d,sizeof(long int);
-printf("Size of a long long int:%d,sizeof(long long int);
-printf("Size of a float:%d,sizeof(float);
+printf("Size of a char:%d",sizeof(char);
+printf("Size of an int:%d",sizeof(int);
+printf("Size of a long int:%d",sizeof(long int);
+printf("Size of a long long int:%d",sizeof(long long int);
+printf("Size of a float:%d",sizeof(float);
 return (0);
 }
 
