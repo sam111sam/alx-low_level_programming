119123fa326a57e9b78eada755ab0182267983fb
sam <samuelnega1@gmail.com>
2022-03-10 15:38:08 -0800

    update

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,6 +6,6 @@
  */
 int main(void)
 {
-fwrite("and that piece of art is useful" - Dora Korpar, 2015-10-19\n",stderr);
+fwrite("and that piece of art is useful" - Dora Korpar, 2015-10-19\n",39,1,stderr);
 return (1);
 }
