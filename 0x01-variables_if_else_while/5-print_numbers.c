7dec2fefd4a6061b1a4103fd32d23f41a3251fef
sam <samuelnega1@gmail.com>
2022-03-14 16:50:12 -0700

    qn 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int num = 1;
+
+	while (num <= 9)
+{
+	putchar(num);
+	num++;
+}
+	putchar('\n');
+	return (0);
+}
