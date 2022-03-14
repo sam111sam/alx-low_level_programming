66828ed0c9e6873f0c78793502b4915639946005
sam <samuelnega1@gmail.com>
2022-03-14 16:21:24 -0700

    qn 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alp = 'a';
+
+	while (alp <= 'z')
+{
+	if (alp == 'e' || alp == 'q')
+{
+	continue;
+}
+	putchar(alp);
+	alp++;
+}
+	putchar('\n');
+	return (0);
+}
