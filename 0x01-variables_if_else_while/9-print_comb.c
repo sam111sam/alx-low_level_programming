f1d07324936fddde11eae59010340b02efbac0fc
sam <samuelnega1@gmail.com>
2022-03-15 01:16:27 -0700

    qn 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int num = 0;
+
+	while (num <= 9)
+{
+	putchar('0' + num);
+	putchar(',');
+	putchar(32);
+	num++;
+}
+	putchar('\n');
+	return (0);
+}
