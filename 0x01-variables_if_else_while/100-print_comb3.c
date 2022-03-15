1e65f1226ea5be454c426ffd3ef8e86a10b00e2c
sam <samuelnega1@gmail.com>
2022-03-15 04:22:46 -0700

    qn 10

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int num1 = 0;
+	int num2 = 1;
+
+	while (num1 <= 9)
+{
+	num2 = 1;
+	while (num2 <= 9)
+{
+	if (num1 < num2 && num1 != num2)
+{
+	putchar(num1 + '0');
+	putchar(num2 + '0');
+	num2++;
+	if (num1 != 8)
+{
+	putchar(',');
+	putchar(32);
+}
+}
+	num2++;
+}
+	num1++;
+}
+	putchar('\n');
+	return (0);
+}
