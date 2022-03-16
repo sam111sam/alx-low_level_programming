ffd397d3c81169a2ea330964ea9af7a03dc4f0f5
sam <samuelnega1@gmail.com>
2022-03-16 10:05:15 -0700

    qn 12

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -18,7 +18,7 @@ while (num1 <= 9)
 	num2 = 0;
 while (num2 <= 9)
 {
-	if (num5 == 1)
+if (num5 == 1)
 break;
 	num4 = 0;
 	num3 = 0;
@@ -34,24 +34,25 @@ while (num4 <= 9)
 	putchar(32);
 	putchar('0' + num3);
 	putchar('0' + num4);
-        if (num1 == 9 && num2 == 8 && num3 == 9 && num4 == 9)
+	if (num1 == 9 && num2 == 8 && num3 == 9 && num4 == 9)
 {
-        num5++;
+	num5++;
 }
 else
 {
-        putchar(','); 
-        putchar(32);
+	putchar(',');
+	putchar(32);
 }
-}num4++;
 }
-        num3++;
+	num4++;
 }
-        num2++;
+	num3++;
 }
-        num1++;
+	num2++;
+}
+	num1++;
 }
 	putchar('\n');
-        return (0);
+	return (0);
 }
 
