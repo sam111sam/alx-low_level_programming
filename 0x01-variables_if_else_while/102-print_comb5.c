0a0bf0396e76adc5cc1ebd5d6af094abdb1ba293
sam <samuelnega1@gmail.com>
2022-03-16 09:55:36 -0700

    qn 12

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -6,33 +6,34 @@
  */
 int main(void)
 {
-        int num1 = 0;
-        int num2 = 0;
-        int num3 = 0;
-        int num4 = 0;
-        int num5 = 0;
+	int num1 = 0;
+	int num2 = 0;
+	int num3 = 0;
+	int num4 = 0;
+	int num5 = 0;
 while (num1 <= 9)
 {
-        num4 = 0;
-        num3 = 0;
-        num2 = 0;
+	num4 = 0;
+	num3 = 0;
+	num2 = 0;
 while (num2 <= 9)
-{if (num5 == 1)
+{
+	if (num5 == 1)
 break;
-        num4 = 0;
-        num3 = 0;
+	num4 = 0;
+	num3 = 0;
 while (num3 <= 9)
 {
-        num4 = 0;
+	num4 = 0;
 while (num4 <= 9)
 {
-        if (num1 < num3 || (num1 == num3 && num2 < num4) )
+	if (num1 < num3 || (num1 == num3 && num2 < num4))
 {
-        putchar('0' + num1);
-        putchar('0' + num2);
-        putchar(32);
-        putchar('0' + num3);
-        putchar('0' + num4);
+	putchar('0' + num1);
+	putchar('0' + num2);
+	putchar(32);
+	putchar('0' + num3);
+	putchar('0' + num4);
         if (num1 == 9 && num2 == 8 && num3 == 9 && num4 == 9)
 {
         num5++;
