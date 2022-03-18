29772c82c727bcc6341c0b613a3ebc591039a3f6
sam <samuelnega1@gmail.com>
2022-03-18 05:39:20 -0700

    qn 0

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ *print_alphabet - print lower case alpha
+ */
+void print_alphabet(void)
+{
+	char a = 'a';
+while (a <= 'z')
+{
+	_putchar(a);
+	a++;
+}
+	_putchar('\n');
+}
