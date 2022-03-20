53bc4d0ff9a4cc27a16fd5e2e7a91696a432c961
sam <samuelnega1@gmail.com>
2022-03-20 04:40:40 -0700

    qn 7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_last_digit - pring the last digit og given number
+ * @c:  the parameter
+ * Return: return the last digit
+ */
+int print_last_digit(int c)
+{
+	if (c % 10 > 0)
+{
+	_putchar('0' + (c % 10));
+	return (c % 10);
+}
+	else
+{
+	_putchar('0' + (-c % 10));
+	return (-c % 10);
+}
+}
