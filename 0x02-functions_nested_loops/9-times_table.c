39579e0e1f0260502e953286920eb3118c189905
sam <samuelnega1@gmail.com>
2022-03-21 05:59:20 -0700

    qn 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -5,15 +5,17 @@
 void times_table(void)
 {
 	int a = 0;
+
 	while (a <= 9)
 {
 	int b = 0;
+
 	while (b <= 9)
 {
-	if (a*b >= 10)
+	if (a * b >= 10)
 {
-	_putchar(((a*b) / 10) + '0');
-	_putchar(((a*b) % 10) + '0');
+	_putchar(((a * b) / 10) + '0');
+	_putchar(((a * b) % 10) + '0');
 	if (b != 9)
 {
 	_putchar(',');
@@ -22,8 +24,11 @@ void times_table(void)
 }
 	else
 {
+	if (b != 0)
+{
 	_putchar(' ');
-	_putchar('0' + a*b);
+}
+	_putchar('0' + a * b);
 	if (b != 9)
 {
 	_putchar(',');
