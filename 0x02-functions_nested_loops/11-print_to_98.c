56124b0f4ed29998fc261f7140916aafccde58f2
sam <samuelnega1@gmail.com>
2022-03-22 07:35:38 -0700

    qn 11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -3,39 +3,41 @@
  * print_to_98 - print number fron n to 98
  * @n: the given number
  */
-void digit( int n);
+void digit(int n);
 void print_to_98(int n)
 {
 	while (n != 98)
-{
-	if (n < 0)
-{
-	_putchar('-');
-	digit(-1*n);
-}
-	else{
-	digit(n);
-}
-	_putchar(',');
-	_putchar(' ');
-	if (n > 98)
-{
-	n--;
-}
-	else
-	n++;
-}
+	{
+		if (n < 0)
+		{
+			_putchar('-');
+			digit(-1 * n);
+		}
+		else
+		{
+			digit(n);
+		}
+		_putchar(',');
+		_putchar(' ');
+		if (n > 98)
+		{
+			n--;
+		}
+		else
+			n++;
+	}
 	_putchar('0' + 9);
 	_putchar('0' + 8);
 	_putchar('\n');
 }
-void digit( int n)
+void digit(int n)
 {
 	if (n < 10)
-{
-	_putchar('0' + n) ;
-	return;
-}
-	digit(n / 10);
-	_putchar('0' + (n % 10));
+	{
+		_putchar('0' + n);
+	} else
+	{
+		digit(n / 10);
+		_putchar('0' + (n % 10));
+	}
 }
