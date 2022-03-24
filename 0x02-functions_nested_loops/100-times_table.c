eff6d11cbbf0e82a09fa365e1eab048d35d8335a
sam <samuelnega1@gmail.com>
2022-03-24 00:48:51 -0700

    qn 12

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,61 @@
+#include "main.h"
+/**
+ * print_times_table - n times table
+ * @n: parameter n
+ */
+void digit(int sum);
+void print_times_table(int n)
+{
+	if (n >= 0 && n <= 15)
+	{
+		int a = 0;
+		while (a <= n)
+		{
+			int b = 0;
+			while (b <= n)
+			{
+				int sum = a * b;
+				if (b != 0)
+                                {
+                                        _putchar(',');
+					if (sum < 10)
+					{
+                                        _putchar(' ');
+					_putchar(' ');
+					_putchar(' ');
+
+					}
+					else if (sum < 100)
+					{
+						_putchar(' ');
+						_putchar(' ');
+
+					}
+					else
+						_putchar(' ');
+
+
+                                }
+				digit(sum);
+				b++;
+			}
+			_putchar('\n');
+			a++;
+		}
+	}
+}
+/**
+ * digit - print all digit
+ * @sum: input parameter
+ */
+	void digit(int sum)
+	{
+if (sum < 10)
+{
+_putchar('0' + sum);
+}else
+{
+digit(sum / 10);
+_putchar('0' + sum % 10);
+}
+}
