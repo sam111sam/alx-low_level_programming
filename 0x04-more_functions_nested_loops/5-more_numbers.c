c1f8b09a70b2d8ab943aeaadf9951aa1aed6ec28
sam <samuelnega1@gmail.com>
2022-04-08 01:18:37 -0700

    qn 5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * more_numbers - print 0 to 9
+ */
+void more_numbers(void)
+{
+	int b = 0;
+
+	while (b <= 9)
+	{
+		int a = 0;
+
+		while (a <= 14)
+		{
+			if (a > 10)
+				_putchar('0' + a / 10);
+			_putchar('0' + a % 10);
+			a++;
+
+		}
+		_putchar('\n');
+
+		b++;
+	}
+}
