a4adca5633fa402b74e5f072d24f26413f2725b5
sam <samuelnega1@gmail.com>
2022-04-11 02:03:51 -0700

    qn 10

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - sinds highest prime
+ * Return: 0 on success
+ */
+int main(void)
+{
+	long int prime = 0;
+	long int num = 612852475143;
+	long int a = 2;
+
+	while (a <= num)
+	{
+		if(num % a == 0)
+		{
+			num = num / a;
+			if (prime <= a)
+				prime = a;
+			a = a - 1;
+		}
+		a++;
+	}
+	printf("%ld", prime);
+	printf("\n");
+	return (0);
+}
