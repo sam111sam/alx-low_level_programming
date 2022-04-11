1beb554a9dbd251bb6e8cf9b34db80afb301cf40
sam <samuelnega1@gmail.com>
2022-04-10 23:47:55 -0700

    qn 7

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * print_diagonal - print diagonal size n
+ * @n: parameter
+ */
+void print_diagonal(int n)
+{
+	int a = 0;
+
+	while (a < n)
+	{
+		int b = 0;
+
+		_putchar('\n');
+		while (b < n)
+		{
+
+			if (a == b)
+			{
+				_putchar('\\');
+
+			}
+			else
+			{
+				if (a > b)
+				_putchar(' ');
+			}
+			b++;
+		}
+		a++;
+	}
+	_putchar('\n');
+}
