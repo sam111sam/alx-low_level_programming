1f7f8c72344989a7ea96aec6c222ac7d6333a731
sam <samuelnega1@gmail.com>
2022-04-19 06:00:19 -0700

    qn 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * _strlen - print slen
+ * @s: parameter
+ * Return: int
+ */
+int _strlen(char *s)
+{
+	int count = 0;
+
+	while (*s != '\0')
+	{
+		count++;
+		s++;
+	}
+	return (count);
+}
+/**
+ * print_rev - print string
+ * @s: parameter
+ */
+void print_rev(char *s)
+{
+	int max = _strlen(s);
+	int i = 0;
+
+	while (i <= max)
+	{
+		_putchar(*(s + max));
+		max--;
+	}
+	_putchar('\n');
+}
