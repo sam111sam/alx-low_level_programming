8cc67bc1b10f41cff374eab8f5e88059b07d1007
sam <samuelnega1@gmail.com>
2022-06-10 00:43:41 -0700

    qn 9

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _strcpy - copy string to buffer
+ * @dest: parameter1
+ * @src: parameter 2
+ * Return: pointer
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	while (*(src + i) != '\n' && i < 97)
+	{
+		dest[i] = *(src + i);
+		i++;
+	}
+	dest[i] = '\n';
+	return (dest);
+}
