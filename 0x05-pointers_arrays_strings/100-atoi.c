104295d2fd9f0ab13034dd4cfaa99868fd4e9642
sam <samuelnega1@gmail.com>
2022-06-16 00:00:40 -0700

    qn 10

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _atoi - change string to integer
+ * @s: parameter
+ * Return: int
+ */
+int _atoi(char *s)
+
+{
+	int res = 0, j = 0, a = 0, i = 0;
+
+	for (; s[i] != '\0'; i++)
+	{
+		if (s[i] >= 48 && s[i] <= 57)
+		{
+			a++;
+			res = res * 10 + (s[i] - '0');
+			if (s[i - 1] == 45)
+				j++;
+		}
+		else
+		{
+			if (res != 0 || (res == 0 && a != 0))
+				break;
+		}
+	}
+if (j != 0)
+	res *= -1;
+return (res);
+}
