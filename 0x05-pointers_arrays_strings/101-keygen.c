c829922482ecbd40883b6748fd3adf3dedc91479
sam111sam <81506247+sam111sam@users.noreply.github.com>
2024-03-02 21:49:50 +0300

    Update 101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -13,8 +13,10 @@ int main(void)
         srand(time(NULL));
         while (sum < 2772)
         {
-                c = (rand() % (126 -0 -1)) + 0;
-                if (sum + c <= 2772)
+                c = (rand() % (126 -33 -1)) + 33;
+                if (2772 - sum >= 33 && 2772 - sum <= 126)
+                       c = 2772 - sum;
+                if (sum + c <= 2772 && (2772 - sum  - c >= 33 || 2772 - sum - c == 0))
                 {
                         sum += c;
                         printf("%c",c);
