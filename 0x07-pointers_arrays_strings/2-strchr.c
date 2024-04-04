d8ca1688c36c30ccce2c25af945faea065c44ef0
sam111sam <samuelnega1@gmail.com>
2024-04-04 15:46:21 -0700

    strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,20 @@
+#include <stddef.h>
+/**
+ * _strchr - char in str finder
+ * @s: string
+ * @c: char to be found
+ *
+ * Return: pointer to c or NUll
+ */
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+
+	while (s[i])
+	{
+		if (s[i] == c)
+			return (&s[i]);
+		i++;
+	}
+	return (NULL);
+}
