4bd080f52d1df35da8bd4f196d356aab65609b94
sam111sam <samuelnega1@gmail.com>
2024-04-09 06:29:24 -0700

    strpbrk

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,28 @@
+#include <stddef.h>
+#include <string.h>
+
+/**
+ * 
+ * _strpbrk - searches a string for any of a set of bytes.
+ * @s: sring given
+ * @accept: char to be found
+ * Return: string
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int i, j;
+	char *p = &s[strlen(s)];
+	for (i = 0; accept[i]; i++)
+	{
+		for (j = 0; accept[j]; j++)
+		{
+			if (accept[i] == s[j] && &s[j] < p)
+			{
+				p = &s[j];
+			}
+		}
+	}
+	if (p < &s[j])
+		return (p);
+	return (NULL);
+}
