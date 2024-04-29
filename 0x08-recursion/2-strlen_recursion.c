e5e369106528ce4d9ac3f13cb7be90a975a69cca
sam111sam <samuelnega1@gmail.com>
2024-04-29 12:40:32 -0700

    strlen

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,13 @@
+/**
+ * _strlen_recursion - compute len
+ * @s: string
+ * Return: len
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return 0;
+	}
+	return 1 + _strlen_recursion(s + 1);
+}
