4c1bf5f316ca4f37412b54fdd3fab52c15ef8233
sam111sam <samuelnega1@gmail.com>
2024-04-29 12:49:49 -0700

    factorial

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,19 @@
+/**
+ * factorial - compute factorial
+ * @n: int
+ * Return: int
+ */
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+
+	if (n == 0)
+	{
+		return (1);
+	}
+	return (n * factorial(n - 1));
+}
+
