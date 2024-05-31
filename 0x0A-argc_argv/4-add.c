d1ff1bea41faf5af1fe1182a0af17561bef86bee
sam111sam <samuelnega1@gmail.com>
2024-05-31 00:25:55 -0700

    cmd arg sum

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,35 @@
+#include <ctype.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - entry
+ * @argc: no. args
+ * @argv: args
+ * Return: 0|1
+ */
+int main(int argc, char *argv[])
+{
+	int i, j, len, result = 0;
+
+	if (argc == 1)
+	{
+		printf("%i\n", result);
+		return (1);
+	}
+	
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0, len = strlen(argv[i]); j < len; j++)
+		{
+			if (!isdigit(argv[i][j]))
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		result += atoi(argv[i]);
+	}
+	printf("%i\n", result);
+	return (0);
+}
