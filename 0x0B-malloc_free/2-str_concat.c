1eb7b6b8afcb2fb96f976c07072f2964ea7ddac8
sam111sam <samuelnega1@gmail.com>
2024-06-02 03:31:34 -0700

    concat str

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,43 @@
+#include <stdlib.h>
+#include <string.h>
+/**
+ * str_concat - concatinate string
+ * @s1: str 1
+ * @s2: str 2
+ * Return: char *
+ */
+char *str_concat(char *s1, char *s2)
+{
+	char *text;
+	int len = 0;
+	int l1 = strlen(s1);
+	int l2 = strlen(s2);
+	int i;
+
+	if (s1 != NULL)
+	{
+		len += l1;
+	}
+	if (s2 != NULL)
+	{
+		len += l2;
+	}
+	text = malloc(len + 1);
+
+	if (text != NULL)
+	{
+		for (i = 0; s1[i]; i++)
+		{
+			text[i] = s1[i];
+		}
+
+		for (i = 0; s2[i]; i++)
+		{
+			text[i + l1] = s2[i];
+		}
+		text[i + l1] = '\0';
+	}
+	return (text);
+}
+
+
