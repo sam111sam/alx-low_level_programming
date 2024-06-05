5f3cb86eff327a11cb6d31d69e5e18a55dd52c7e
sam111sam <samuelnega1@gmail.com>
2024-06-05 03:16:49 -0700

    concatinate n str

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,49 @@
+#include <stdlib.h>
+#include <string.h>
+/**
+ * string_nconcat - concatinate str
+ * @s1: txt 1
+ * @s2: txt 2
+ * @n: no s2 to be copy
+ * Return: char *
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *text;
+	unsigned int len = 0, i, l1 = 0, l2 = 0;
+
+	if (s1)
+	{
+		l1 = strlen(s1);
+		len += l1;
+	}
+
+	if (s2 != NULL)
+	{
+		l2 = strlen(s2);
+
+		if (strlen(s2) <= n)
+			len += l2;
+		else
+			len += n;
+	}
+
+	text = malloc(sizeof(char) * len);
+
+	if (text)
+	{
+		for (i = 0; i < l1; i++)
+		{
+			text[i] = s1[i];
+		}
+
+		for (i = 0; i < n && i <= l2; i++)
+		{
+			text[l1 + i] = s2[i];
+		}
+
+		if (n < l2)
+			text[l1 + n] = '\0';
+	}
+	return (text);
+}
