5a354016611d4035dafc9f014963bba263c09ea2
sam111sam <samuelnega1@gmail.com>
2024-06-15 05:23:14 -0700

    error fix

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -9,17 +9,15 @@ void free_dog(dog_t *d)
 {
 	if (d)
 	{
+		if (d->name)
+		{
+			free(d->name);
+		}
+
+		if (d->owner)
+		{
+			free(d->owner);
+		}
 		free(d);
-		return;
-	}
-
-	if (d->name)
-	{
-		free(d->name);
-	}
-
-	if (d->owner)
-	{
-		free(d->owner);
 	}
 }
