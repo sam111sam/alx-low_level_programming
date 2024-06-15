65ef5e995fc10b8c4a128136af01649391754ac5
sam111sam <samuelnega1@gmail.com>
2024-06-15 02:35:55 -0700

    error fix

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -9,7 +9,11 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	d->name = name;
+	if (name != NULL)
+		d->name = name;
+
 	d->age = age;
-	d->owner = owner;
+
+	if (owner != NULL)
+		d->owner = owner;
 }
