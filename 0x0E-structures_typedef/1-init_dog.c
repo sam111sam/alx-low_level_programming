ff6881bf469f2326002a0d27dd749ea471da3eae
sam111sam <samuelnega1@gmail.com>
2024-06-15 02:30:14 -0700

    intialize struct

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,15 @@
+#include "dog.h"
+/**
+ * init_dog - intialize struct
+ * @d: ptr to struct dog
+ * @name: dog name
+ * @age: dog age
+ * @owner: owner name
+ * Return: void
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
