1601eb4b6923e0ac0a7179e6332caef7f774d717
sam111sam <samuelnega1@gmail.com>
2024-06-15 02:44:06 -0700

    error fix

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,4 +1,5 @@
 #include "dog.h"
+#include <stddef.h>
 /**
  * init_dog - intialize struct
  * @d: ptr to struct dog
@@ -10,7 +11,9 @@
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d != NULL)
+	{
 		d->name = name;
 		d->age = age;
 		d->owner = owner;
+	}
 }
