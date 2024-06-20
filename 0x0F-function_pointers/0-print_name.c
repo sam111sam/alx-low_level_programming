8ea249d0cb9ea9080a5d1de5d152f09a8dc7df3d
sam111sam <samuelnega1@gmail.com>
2024-06-20 04:30:09 -0700

    print a name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,10 @@
+/**
+ * print_name - print string using d/t func
+ * @name: string
+ * @f: function pointer
+ * Return: void
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,4 @@
+#ifndef fun_p
+#define fun_p
+void print_name(char *name, void (*f)(char *));
+#endif /*fun_p */
