11704d555396b8a69b384ee00a2511d905772043
sam111sam <samuelnega1@gmail.com>
2024-06-20 05:03:17 -0700

    task 2

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,23 @@
+/**
+ * int_index - return index if num is found
+ * @array: array of int
+ * @size: size of array
+ * @cmp: ptr to function
+ * Return: int
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (!array || !cmp || size < 0)
+		return (-1);
+
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]))
+		{
+			return (i);
+		}
+	}
+	return (-1);
+}
