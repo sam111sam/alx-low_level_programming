fe663ecf666c298b86c0d6c40202e84e8684eeaf
sam111sam <samuelnega1@gmail.com>
2024-06-22 04:13:33 -0700

    args separated by sep

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdarg.h>
+/**
+ * print_numbers - print args separated by char
+ * @separator: separate no.
+ * @n: no. os args
+ * Return: void
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list nums;
+	unsigned int i;
+
+	va_start(nums, n);
+
+	for (i = 0; i < n; i++)
+	{
+
+		if (separator && i < n - 1)
+		{
+			printf("%i%c ", va_arg(nums, int), *separator);
+		}
+		else
+		{
+			printf("%i", va_arg(nums, int));
+		}
+	}
+	printf("\n");
+}
