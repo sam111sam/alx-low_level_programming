04efd91462c6eac79dd6a7c5475045ab633bb5fa
sam111sam <samuelnega1@gmail.com>
2024-07-18 07:03:02 -0700

    adding list element at the beg

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,27 @@
+#include <string.h>
+#include <stdlib.h>
+#include "lists.h"
+/**
+ * add_node - add athe beg of list
+ * @head: addr of head
+ * @str: eleemet
+ * Return: list_t *
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *temp = malloc(sizeof(list_t));
+
+	if (!temp)
+		return (temp);
+	if (*str != 0)
+	{
+		temp->len = strlen(str);
+		temp->str = strdup(str);
+	}
+	temp->next = NULL;
+
+	temp->next = (*head);
+	*head = temp;
+	return (temp);
+}
+
