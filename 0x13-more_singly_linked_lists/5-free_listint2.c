0cc258d7315a98df1f0774c08fdc923fbec1ee59
sam111sam <samuelnega1@gmail.com>
2024-07-23 08:33:52 -0700

    free

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -6,8 +6,12 @@
  */
 void free_listint2(listint_t **head)
 {
-	listint_t *temp = *head;
-	listint_t *current = *head;
+	listint_t *temp;
+	listint_t *current;
+
+	if (!head)
+		return;
+	temp = current = *head;
 
 	while (current)
 	{
