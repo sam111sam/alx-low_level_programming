414928bcbaff17483b2cac079d716aa28c3457b3
sam111sam <samuelnega1@gmail.com>
2024-07-24 03:59:33 -0700

    fix break after insert

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -27,6 +27,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	{
 		new->next = *head;
 		*head = new;
+		return (new);
 	}
 	for (i = 0; temp && i < idx - 1; i++)
 	{
