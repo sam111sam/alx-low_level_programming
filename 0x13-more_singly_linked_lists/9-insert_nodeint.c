851abfcc05179334658645447dd2f5964b2b1c91
sam111sam <samuelnega1@gmail.com>
2024-07-24 03:55:32 -0700

    fix no list passed

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -9,8 +9,13 @@
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	listint_t *temp = *head;
-	listint_t *new = malloc(sizeof(listint_t));
+	listint_t *new;
 	unsigned int i;
+
+	if (!head)
+		return (NULL);
+
+	new = malloc(sizeof(listint_t));
 
 	if (!new)
 		return (NULL);
