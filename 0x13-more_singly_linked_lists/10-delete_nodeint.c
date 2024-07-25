94ecafd4476f76ba9ade86c239a6aff6d4c2818d
sam111sam <samuelnega1@gmail.com>
2024-07-25 06:12:02 -0700

    fix the no of iteration

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -18,7 +18,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		temp = *head;
 		*head = (*head)->next;
 		free(temp);
-		return (0);
+		return (1);
 	}
 	for (i = 0; i < index && temp; i++)
 	{
